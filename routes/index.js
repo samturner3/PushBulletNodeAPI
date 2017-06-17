b3b047a4e78ac6f1023075ee52bb3078794c4b60
samturner3 <sam.turner@students.mq.edu.au>
2017-06-17 15:58:41 +1000

    Moved API keys to Env vars

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var readline = require('readline');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', text: 'Hello World' });
